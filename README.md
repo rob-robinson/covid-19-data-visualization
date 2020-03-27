04cd3d9805d21448ea333fafccb78ab10508c721
Rob Robinson <robr@uoregon.edu>
2020-03-27 01:14:45 -0700

    update code
    next commit will have a refactor

---
+++ README.md
@@ -0,0 +1,13 @@
+# CoViD-19 Data Visualization
+
+#### Running Locally
+
+```bash
+
+python3 -m venv my_p3
+
+source my_p3/bin/activate
+
+pip install -r requirements.txt
+
+```
--- src/src/Main.py
+++ src/src/Main.py
@@ -1,27 +1,35 @@
 import pandas as pd
 import datetime as dt
 
-# US Population --> 329,500,000
+# population data from https://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States_by_population_density
+# and https://www.census.gov/data/datasets/time-series/demo/popest/2010s-state-total.html ... specifically:
+# https://www2.census.gov/programs-surveys/popest/tables/2010-2019/state/totals/nst-est2019-01.xlsx
 
 DATA_DIR = '/Users/robrobinson/develop/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/'
 
 # from 03-10-2020 - 03-22-2020: do it the old way ...
 model_one_dates = pd.date_range(start="2020-03-10", end="2020-03-21").to_pydatetime().tolist()
 
-all_data = pd.DataFrame()
+# initial load of states and popluations
+all_data = pd.read_csv('../data/us_states_populations.csv')
 
 for date in model_one_dates:
 
     date_str = date.strftime("%m-%d-%Y")
 
-    print("OLD:" + date_str)
-
     # open the proper file
     CONFIRMED = DATA_DIR + date_str + '.csv'
-    df = pd.read_csv(CONFIRMED, skipinitialspace=False, usecols=['Province/State', 'Country/Region', 'Confirmed', 'Deaths'])
 
-    df.rename(columns={'Confirmed': 'Confirmed-' + date_str + ''}, inplace=True)
-    df.rename(columns={'Deaths': 'Deaths-' + date_str + ''}, inplace=True)
+    try:
+        df = pd.read_csv(CONFIRMED, skipinitialspace=False,
+                         usecols=['Province/State', 'Country/Region', 'Confirmed', 'Deaths'])
+
+    except OSError as e:
+        print('Note: '+date_str+'.csv was not found ... ' + e.strerror)
+        continue  # will skip the rest of the block and move to next file
+
+    df.rename(columns={'Confirmed': 'Confirmed-' + date_str}, inplace=True)
+    df.rename(columns={'Deaths': 'Deaths-' + date_str}, inplace=True)
 
     df.rename(columns={'Province/State': 'Province_State'}, inplace=True)
     df.rename(columns={'Country/Region': 'Country_Region'}, inplace=True)
@@ -31,8 +39,6 @@ for date in model_one_dates:
     state_data = us_data.groupby('Province_State', as_index=False)
 
     tmp_df = state_data.sum()
-
-    # print(tmp_df.info())
 
     if all_data.empty:
         all_data = tmp_df.copy()
@@ -47,12 +53,11 @@ for date in model_two_dates:
 
     date_str = date.strftime("%m-%d-%Y")
 
-    print("NEW:" + date_str)
-
     CONFIRMED = DATA_DIR + date_str + '.csv'
 
     try:
-        df = pd.read_csv(CONFIRMED, skipinitialspace=False, usecols=['Province_State', 'Country_Region', 'Confirmed', 'Deaths'])
+        df = pd.read_csv(CONFIRMED, skipinitialspace=False,
+                         usecols=['Province_State', 'Country_Region', 'Confirmed', 'Deaths'])
 
     except OSError as e:
         print('Note: '+date_str+'.csv was not found ... ' + e.strerror)
@@ -72,4 +77,16 @@ for date in model_two_dates:
     else:
         all_data = pd.merge(all_data, tmp_df, on='Province_State')
 
-print(all_data)
+
+# I wonder if I can create new columns in all_data for the following calculations:
+# -- deaths per confirmed
+# -- confirmed per million people
+# -- deaths per million people
+
+# compression_opts = dict(method='zip', archive_name='out.csv')
+#
+# all_data.to_csv('out.zip', index=False,compression=compression_opts)
+
+# compression_opts = dict(method='zip', archive_name='out.csv')
+
+all_data.to_csv('out.csv')
